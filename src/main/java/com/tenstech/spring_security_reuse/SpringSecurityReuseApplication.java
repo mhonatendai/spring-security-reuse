a71b9b727bc8ad5a98ac2e75bfaa77fc7d12b2f9
Tendai E. Mhona <mhonatendai@gmail.com>
2025-03-17 15:29:18 -0400

    init

---
+++ src/main/java/com/tenstech/spring_security_reuse/SpringSecurityReuseApplication.java
@@ -0,0 +1,13 @@
+package com.tenstech.spring_security_reuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityReuseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenstech/spring_security_reuse/SpringSecurityReuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenstech.spring_security_reuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
